465b3c7b7651f0c665633695dccc043712eb399a
Mitch Dzugan <mitchdzugan@gmail.com>
2026-01-08 16:21:39 -0600

    rough impl with old comments

--- flake.nix
+++ flake.nix
@@ -11,18 +11,31 @@
     flake-utils,
     ...
   }:
-    let mkPkg = luaPkgs: luaPkgs.buildLuarocksPackage rec {
-      pname = "ctx";
-      version = "0.0.1-0";
-      src = ./.;
-      knownRockspec = ./rockspecs/${pname}-${version}.rockspec;
-      disabled = (luaPkgs.luaOlder "5.1") || (luaPkgs.luaAtLeast "5.4");
-    }; in { mkPkg = mkPkg; } // flake-utils.lib.eachDefaultSystem (
+    let
+      mkLua = (luaPkgs: (luaPkgs.lua.withPackages (lp: with lp; [
+        busted
+        fennel
+      ])));
+      mkPkg = luaPkgs: luaPkgs.buildLuarocksPackage rec {
+        pname = "ctx";
+        version = "0.0.1-0";
+        src = ./.;
+        buildInputs = [(mkLua luaPkgs)];
+        preBuild = ''
+          rm dist/ctx.lua
+          fennel --compile src/index.fnl > dist/ctx.lua
+        '';
+        knownRockspec = ./rockspecs/${pname}-${version}.rockspec;
+        disabled = (luaPkgs.luaOlder "5.1") || (luaPkgs.luaAtLeast "5.4");
+      }; in { mkPkg = mkPkg; } // flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         luaPkgs = pkgs.lua5_1.pkgs;
         nativeBuildInputs = with pkgs; [];
-        buildInputs = with pkgs; [];
+        buildInputs = with pkgs; [
+            (mkLua pkgs.lua5_1.pkgs)
+            rlwrap
+          ];
       in {
         packages.default = mkPkg pkgs.lua5_1.pkgs;
         devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
